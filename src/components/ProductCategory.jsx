b840fb14766165235c1f46ab63a2766f8fe93be2
pyj <yepark98@gmail.com>
2023-03-02 22:23:35 +0900

    stock 구현

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+
+import { stockState } from "./../atoms";
 
 const ProductCategory = (props) => {
   const { category, items } = props;
+  const [inStockOnly, setinStockOnly] = useRecoilState(stockState);
+
+  const filterStock = inStockOnly
+    ? items.filter((item) => item.stocked === inStockOnly)
+    : items;
 
   return (
     <tbody>
       <tr>
         <td>{category}</td>
       </tr>
-      {items.map((item, index) => (
+      {filterStock.map((item, index) => (
         <tr key={index}>
           <td>{item.name}</td>
           <td>{item.price}</td>
